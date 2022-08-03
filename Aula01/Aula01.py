66814fcecf879f864eec5ac5c233c3f4d6b3778d
Isael Pereira de Oliveira <isaelpereiraoliveira@gmail.com>
2022-08-02 21:57:37 -0300

    Initial commit

---
+++ Aula01/Aula01.py
@@ -0,0 +1,11 @@
+larguraG = 5;
+profundidadeG = 10;
+areaG = larguraG * profundidadeG;
+
+larguraT = 50;
+profundidadeT = 30;
+areaT = larguraT * profundidadeT;
+
+ocupacao = (areaG / areaT) * 100;
+print(ocupacao);
+
