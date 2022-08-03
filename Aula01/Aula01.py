9b17e4e1d163f5669d744876dd12b0bed8b67dd4
Isael Pereira de Oliveira <isaelpereiraoliveira@gmail.com>
2022-08-03 01:50:30 -0300

    update-do-codigo

--- Aula01/Aula01.py
+++ Aula01/Aula01.py
@@ -1,11 +1,13 @@
-larguraG = 5;
-profundidadeG = 10;
-areaG = larguraG * profundidadeG;
+largura_g = float(input('Insira a Largura da Garagem: '))
+profundidade_g = float(input('Insira a Profundidade da Garagem: '))
+#Calculo area da Garagem
+area_g = largura_g * profundidade_g
 
-larguraT = 50;
-profundidadeT = 30;
-areaT = larguraT * profundidadeT;
+largura_t = float(input('Insira a Largura do Terreno: '))
+profundidade_t = float(input('Insira a Profundidade do Terreno: '))
+#Calculo area do terreno
+area_t = largura_t * profundidade_t
 
-ocupacao = (areaG / areaT) * 100;
-print(ocupacao);
-
+#Calculo porcentagem de ocupação da garagem dentro do terreno
+ocupacao = (area_g/area_t) * 100
+print('A garagem ocupa um total de:', ocupacao, '% de todo terreno')
